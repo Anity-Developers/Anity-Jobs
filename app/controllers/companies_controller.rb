2798abb4fbf5f70150dfa375d6440db7f5da9481
Tresor11 <bireketresor@gmail.com>
2022-05-07 08:43:47 +0200

    add the company form

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,5 +1,31 @@
 class CompaniesController < ApplicationController
+  before_action :require_login
+
+  def index
+    @companies = Company.all
+  end
+
   def new
     @company = Company.new
+  end
+
+  def create
+    @company = Company.create(company_params)
+    if @company.save
+      flash[:notice] = "Company created successfully."
+      redirect_to companies_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @company = Company.find(params[:id])
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:new, :create]
+  resources :users, only: [:index]
   resources :companies
   # Defines the root path route ("/")
   root "users#index"
