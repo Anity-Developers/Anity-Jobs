a3b791438645b88cc0b8973ec3dfda43093009c2
vic778 <victoiremmanuelbarh@gmail.com>
2022-05-26 13:22:24 +0300

    Permit the admin to manage all actions

--- app/policies/job_policy.rb
+++ app/policies/job_policy.rb
@@ -3,9 +3,26 @@ class JobPolicy < ApplicationPolicy
     user.has_permission?(:create, :job)
   end
 
+  def show?
+    user.has_permission?(:show, :job)
+  end
+
   def new?
     create?
   end
+
+  def update?
+    user.has_permission?(:update, :job)
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    user.has_permission?(:destroy, :job)
+  end
+  
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     # def resolve
