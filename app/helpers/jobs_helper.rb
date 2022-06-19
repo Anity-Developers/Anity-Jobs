dd5147ffdb5781d74c9df3616e191ffbaf61fa55
Tresor11 <bireketresor@gmail.com>
2022-06-19 10:47:54 +0200

    make the job preview responsive

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -4,4 +4,9 @@ module JobsHelper
     # check if @job was created in the last 10 days
     job.created_at > 10.days.ago
   end
+
+  def format_date(job)
+    job.created_at.strftime("%b %d")
+  end
+
 end
