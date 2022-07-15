021d7da8dd3cc33d9a4fc68980e821064f5b839c
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-15 17:47:57 +0300

    Exptected to ne be valid if the name is not unique

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,6 +3,20 @@ require 'rails_helper'
 RSpec.describe Category, type: :model do
   it { is_expected.to have_many(:jobs) }
 
-  describe "when"
+  describe "when name is not present" do
+    before { @category = Category.new(name: "") }
+    it { is_expected.not_to be_valid }
+  end
+
+  describe "when name is present" do
+    before { @category = Category.new(name: "Category") }
+    it { is_expected.to be_valid }
+  end
+
+  describe "when name is not unique" do
+    before { @category = Category.create(name: "Category") }
+    before { @category2 = Category.new(name: "Category") }
+    it { is_expected.not_to be_valid }
+  end
 
 end
