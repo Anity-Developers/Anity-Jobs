b531e86e78d73defe357947728edae26b1391bf8
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-15 17:39:59 +0300

    Describe the presence of the name

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to have_many(:jobs) }
+
+  describe "when"
+
 end
