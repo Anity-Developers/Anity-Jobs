aa99d1512c605a17062d38a60ec2a4e81df30491
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-15 17:58:50 +0300

    Describe the presence of status

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,15 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   it { is_expected.to have_many(:jobs) }
+  it { is_expected.to validate_presence_of(:name) }
 
   describe "when name is not present" do
     before { @category = Category.new(name: "") }
     it { is_expected.not_to be_valid }
-  end
-
-  describe "when name is present" do
-    before { @category = Category.new(name: "Category") }
-    it { is_expected.to be_valid }
   end
 
   describe "when name is not unique" do
