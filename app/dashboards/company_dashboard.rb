b20ddea196ae6c1f6309187e4c9406e9f8ea4b18
vic778 <victoiremmanuelbarh@gmail.com>
2022-07-25 22:34:48 +0300

    Associate company to locatiopn

--- app/dashboards/company_dashboard.rb
+++ app/dashboards/company_dashboard.rb
@@ -8,6 +8,7 @@ class CompanyDashboard < Administrate::BaseDashboard
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
+    location: Field::BelongsTo,
     description: Field::Text,
     jobs: Field::HasMany,
     id: Field::Number,
@@ -22,6 +23,7 @@ class CompanyDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
+    location
     description
     jobs
     id
@@ -31,6 +33,7 @@ class CompanyDashboard < Administrate::BaseDashboard
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = %i[
+    location
     description
     jobs
     id
@@ -43,6 +46,7 @@ class CompanyDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
+    location
     description
     jobs
     name
