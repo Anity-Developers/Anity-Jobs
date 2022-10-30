f8d3df669ee8be0a65db0f2b3213bd6acc671c35
Tresor11 <bireketresor@gmail.com>
2022-10-30 15:07:47 +0400

    Revert "update the algolia app"
    This reverts commit d802538d8b8935bf8a42b31b0e1c3a19ad617d83.

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,6 +1,6 @@
 AlgoliaSearch.configuration = {
-  application_id: 'AYILOV5OJE',
-  api_key: 'e50fc3654c7d5760baeade377b10d1e1',
+  application_id: 'FMAO7DRALI',
+  api_key: 'd9a9f4d270b00470e0287e10d14b212a',
   connect_timeout: 2,
   receive_timeout: 30,
   send_timeout: 30,
