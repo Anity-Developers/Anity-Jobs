3aa3d231eab95ab5abb87b4b674971812ca554c7
vic778 <victoiremmanuelbarh@gmail.com>
2022-10-30 12:59:40 +0400

    Login a user before any action

--- spec/requests/companies_spec.rb
+++ spec/requests/companies_spec.rb
@@ -5,20 +5,20 @@ RSpec.describe "Companies", type: :request do
   include Features::ClearanceHelpers
 
   context "when as an admin" do
-    # let!(:user) { create(:user, :admin) }
-    # let!(:location) { create(:location) }
-    # let!(:company) { create(:company, location: location) }
+    let!(:user) { create(:user, :admin) }
+    let!(:location) { create(:location) }
+    let!(:company) { create(:company, location: location) }
     
-    # before do
-    #   sign_in(FactoryBot.create(:user, :admin))
-    # end
+    before do
+      sign_in(FactoryBot.create(:user, :admin))
+    end
 
-    # describe "GET /companies" do
-    #   it "works! (now write some real specs)" do
-    #     get companies_path
-    #     expect(response).to have_http_status(200)
-    #   end
-    # end
+    describe "GET /companies" do
+      it "works! (now write some real specs)" do
+        get companies_path
+        expect(response).to have_http_status(200)
+      end
+    end
      
   end
   
