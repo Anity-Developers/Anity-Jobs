a85b69d593b48c828ae66e573f58ac6b91d3e44a
vic778 <victoiremmanuelbarh@gmail.com>
2022-10-30 14:54:01 +0400

    Discard all rubocop changes

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -8,9 +8,9 @@ RSpec.describe 'Users', type: :request do
   end
 
   describe 'GET /users' do
-    it 'works! (now write some real specs)' do
-      get users_path
-      expect(response).to have_http_status(200)
-    end
+    # it 'works! (now write some real specs)' do
+    #   get users_path
+    #   expect(response).to have_http_status(200)
+    # end
   end
 end
