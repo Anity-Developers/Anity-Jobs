cb4260b76dd29afd18bb74340992642ec70cfb86
vic778 <victoiremmanuelbarh@gmail.com>
2022-11-18 15:56:43 +0400

    Create method to get all josb created a week

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,4 +5,8 @@ class DashboardController < ApplicationController
     @top_categories = Category.joins(:jobs).group(:id).order("COUNT(jobs.id) DESC").limit(3)
     @top_locations = Location.joins(:jobs).group(:id).order("COUNT(jobs.id) DESC").limit(3)
   end
+
+  def weekly
+    @jobs_week = current_user.jobs.where("created_at > ?", 1.week.ago)
+  end
 end
