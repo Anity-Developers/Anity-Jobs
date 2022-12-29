6efe6570374f91d7d70ad38c203b59b77d253ce9
vic778 <victoiremmanuelbarh@gmail.com>
2022-12-29 22:54:51 +0400

    Remove the update method in the migration

--- db/migrate/20221227114112_add_view_to_job.rb
+++ db/migrate/20221227114112_add_view_to_job.rb
@@ -1,11 +1,5 @@
 class AddViewToJob < ActiveRecord::Migration[7.0]
   def change
     add_column :jobs, :views, :integer, default: 0
-
-    reversible do |dir|
-      dir.up do
-        Job.update_all(views: 0)
-      end
-    end
   end
 end
