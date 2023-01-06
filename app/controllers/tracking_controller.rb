14516a43f106e9cd5d9ee1d6480429025aa7d02b
vic778 <victoiremmanuelbarh@gmail.com>
2023-01-06 16:51:37 +0400

    Comment the method to not execute the sidekiq class

--- app/controllers/tracking_controller.rb
+++ app/controllers/tracking_controller.rb
@@ -1,8 +1,8 @@
 class TrackingController < JobsController
   skip_before_action :verify_authenticity_token
 
-  def send_notification
-    scheduler = TrackerTimeScheduler.new
-    scheduler.perform(params[:job_id])
-  end
+  # def send_notification
+  #   scheduler = TrackerTimeScheduler.new
+  #   scheduler.perform(params[:job_id])
+  # end
 end
