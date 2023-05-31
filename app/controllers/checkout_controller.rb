f8dee87fb101a3cd487ae7e1cd7d48029f4aabdc
vic778 <victoiremmanuelbarh@gmail.com>
2023-05-31 14:01:22 +0400

    Create the stripe item

---
+++ app/controllers/checkout_controller.rb
@@ -0,0 +1,40 @@
+class CheckoutController < ApplicationController
+  def create
+    job = Job.find_by_id(params[:id])
+    @session = Stripe::Checkout::Session.create({
+                                        customer: current_user&.customer&.stripe_customer_id,
+                                        payment_method_types: ['card'],
+                                        line_items: [{
+                                                name: job.title,
+                                                amount: 100,
+                                                currency: 'usd',
+                                                quantity: 1,
+                                                  }],
+                                        allow_promotion_codes: true,
+                                        mode: 'payment',
+                                                  # discounts: [{
+                                                  #   coupon: '{{COUPON_ID}}',
+                                                  # }],
+                                        # success_url: "#{success_url}?session_id={CHECKOUT_SESSION_ID}",
+                                        success_url: root_url,
+                                        cancel_url: cancel_url
+    })
+   
+    redirect_to @session.url, allow_other_host: true
+  end
+
+  def success
+    if params[:session_id].present?
+      session[:cart] = []
+    #   @session_with_expand = Stripe::Checkout::Session.retrieve({ id: params[:session_id], expand: ['line_items'] })
+    #   @session_with_expand.line_items.data.each do |line_item|
+    #     item = Item.find_by(stripe_product_id: line_item.price.product)
+    #     item.increment!(:sales_count)
+    #   end
+    else
+      redirect_to cancel_path, alert: 'Transaction was cancelled'
+    end
+  end
+
+  def cancel; end
+end
