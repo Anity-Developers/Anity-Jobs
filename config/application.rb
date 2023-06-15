6587608ecd96c41ce38d2886e1e4ada50794ddc1
Alfredbis29 <Kalumunabisimwa5@gmail.com>
2023-06-15 16:14:32 +0200

    set up sidekiq

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module AnityJobs
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.active_job.queue_adapter = :sidekiq
 
     # Configuration for the application, engines, and railties goes here.
     #
